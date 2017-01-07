51c4e118e053c10bd743ce7f06892244e53d1d52
Vav <vav@Vavs-Mac.local>
2017-01-07 11:31:40 +0100

    Initial Commit

---
+++ RSSiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSiOS
+//
+//  Created by Vav on 07/01/2017.
+//  Copyright © 2017 Vav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSiOS
+//
+//  Created by Vav on 07/01/2017.
+//  Copyright © 2017 Vav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
